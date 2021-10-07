0cbccf14dbddae9fc7eeeb074c66c768945bfe7e
nkhandare244 <90832496+nkhandare244@users.noreply.github.com>
2021-10-07 15:36:26 +0530

    Add files via upload

---
+++ simplemavenproject/src/test/java/simplemavenproject/Apptest.java
@@ -0,0 +1,14 @@
+package simplemavenproject;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Apptest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
